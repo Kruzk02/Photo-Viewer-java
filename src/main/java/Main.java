48f0e88ad9bfab816315ce2aae62d62df98b98da
thienphuc <thienphuc484@gmail.com>
2024-09-01 12:42:24 +0700

    Update main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import com.formdev.flatlaf.FlatLightLaf;
 import gui.MainGUI;
 
 import javax.swing.*;
@@ -5,6 +6,7 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
+        FlatLightLaf.setup();
         SwingUtilities.invokeLater(new MainGUI());
     }
 
