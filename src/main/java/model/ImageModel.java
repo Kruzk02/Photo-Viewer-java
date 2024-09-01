5429ed0da160fc337fcc7c45026e846de89fddbb
thienphuc <thienphuc484@gmail.com>
2024-09-01 12:43:34 +0700

    Add ImageModel

---
+++ src/main/java/model/ImageModel.java
@@ -0,0 +1,34 @@
+package model;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+public class ImageModel {
+
+    private BufferedImage image;
+    private File file;
+
+    public ImageModel() {
+    }
+
+    public ImageModel(BufferedImage image, File file) {
+        this.image = image;
+        this.file = file;
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    public void setImage(BufferedImage image) {
+        this.image = image;
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+}
