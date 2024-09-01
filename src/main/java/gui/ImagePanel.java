71a37690781f72db943469b75934785d28670cb7
thienphuc <thienphuc484@gmail.com>
2024-09-01 13:38:52 +0700

    Update ImagePanel

--- src/main/java/gui/ImagePanel.java
+++ src/main/java/gui/ImagePanel.java
@@ -1,36 +1,36 @@
 package gui;
 
-import javax.imageio.ImageIO;
+import model.ImageModel;
+
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
 public class ImagePanel extends JPanel {
 
-    private final BufferedImage image;
-    public ImagePanel() {
-        try {
-            image = ImageIO.read(new File(""));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    private final ImageModel model;
+
+    public ImagePanel(ImageModel model) {
+        this.model = model;
+    }
+
+    public void setPreferredSize(int width, int height) {
+        this.setPreferredSize(new Dimension(width,height));
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        if (image != null) {
+        if (model.getImage() != null) {
             Graphics2D g2d = (Graphics2D) g;
+
             g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
             g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
 
             int panelWidth = getWidth();
             int panelHeight = getHeight();
 
-            int imgWidth = image.getWidth();
-            int imgHeight = image.getHeight();
+            int imgWidth = model.getImage().getWidth();
+            int imgHeight = model.getImage().getHeight();
 
             double aspectRatio = (double) imgWidth / imgHeight;
 
@@ -44,7 +44,7 @@ public class ImagePanel extends JPanel {
 
             int x = (panelWidth - newWidth) / 2;
             int y = (panelHeight - newHeight) / 2;
-            g2d.drawImage(image, x, y, newWidth, newHeight, this);
+            g2d.drawImage(model.getImage(), x, y, newWidth, newHeight, this);
         }
     }
 
