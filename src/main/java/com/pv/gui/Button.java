3148cfd9c0d46c2cf32efcadcd87637ad0d31b38
thienphuc <thienphuc484@gmail.com>
2024-09-10 13:18:42 +0700

    Add Button class

---
+++ src/main/java/com/pv/gui/Button.java
@@ -0,0 +1,32 @@
+package com.pv.gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class Button extends JButton {
+    public Button(String text) {
+        super(text);
+        this.setBorderPainted(false);
+        this.setFocusPainted(false);
+        this.setOpaque(true);
+        this.setBackground(Color.WHITE);
+        this.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseExited(MouseEvent e) {
+                setBackground(Color.WHITE);
+            }
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                setBackground(Color.LIGHT_GRAY);
+            }
+        });
+        this.setVisible(false);
+    }
+
+    @Override
+    public void setPreferredSize(Dimension preferredSize) {
+        super.setPreferredSize(preferredSize);
+    }
+}
